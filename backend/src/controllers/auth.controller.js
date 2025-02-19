e20906bbc4c093c4b65b98c4d453bfba3c369b64
Oskar Basiaga <basiagaoskar@gmail.com>
2025-02-19 23:24:04 +0100

    implemented login and logout route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,8 +3,8 @@ import { generateToken } from "../lib/utils.js"
 import bcrypt from "bcryptjs"
 
 export const signup = async (req, res) => {
-    const {fullName, email, password} = req.body
-    
+    const { fullName, email, password } = req.body
+
     try {
         if (!fullName || !email || !password) {
             return res.status(400).json({ message: "All fields are required" })
@@ -14,7 +14,7 @@ export const signup = async (req, res) => {
             return res.status(400).json({ message: "Password must be at least 6 characters" })
         }
 
-        const user = await User.findOne({email})
+        const user = await User.findOne({ email })
 
         if (user) return res.status(400).json({ message: "User already exists" })
 
@@ -46,10 +46,42 @@ export const signup = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    res.send("login route")
+export const login = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+
+        generateToken(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic
+        })
+
+    } catch (error) {
+        console.log("Error in the signup controller", error.message)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
 }
 
 export const logout = (req, res) => {
-    res.send("logout route")
+    try {
+        res.cookie("jwt", "", { maxAge: 0})
+        res.status(200).json({ message: "Logged out successfully" })
+
+    } catch (error) {
+        console.log("Error in the signup controller", error.message)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
 }
